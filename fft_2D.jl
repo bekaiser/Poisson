68003b6ec5b97ecb479e87d9235c1d18b3f9c7ae
Bryan Kaiser <bkaiser@mit.edu>
2017-06-10 11:52:24 -0600

    Create fft_2D.jl

--- fft_2D.jl
+++ fft_2D.jl
@@ -1,12 +1,12 @@
 # Derivatives, spectral anti-derivatives, and de-aliasing by discrete Fourier
 # transforms in 2D. 
 # Bryan Kaiser 
-# 3/2/17
+# 3/25/17
 
 using PyCall
 using PyPlot
-@pyimport numpy as np
-@pyimport pylab as py
+#@pyimport numpy as np
+#@pyimport pylab as py
 
 
 # =============================================================================
@@ -61,20 +61,15 @@ function power_spectrum_2D(S::Array{Float64,2},Kmag::Array{Float64,2})
 	return S_vec, Kmag_vec_union;
 end
 
-function dealias(U::Array{Complex{Float64},2},V::Array{Complex{Float64},2},Kmag::Array{Float64,2})
-	# 2/3 rule padding for de-aliasing a quadratic signal via dst
-	for j = 1:(size(Kmag,1)*size(Kmag,2)) # 2/3 rule
-		if abs(Kmag[j]) >= max(size(Kmag,1),size(Kmag,2))/3.0*Kmag[1,2]; 
-			U[j] = 0.0+0.0im; V[j] = 0.0+0.0im; # Fourier space variables
-		end
-	end
-	uv = real(ifft(U,1:2)).*real(ifft(V,1:2));
+function dealias(U::Array{Complex{Float64},2},V::Array{Complex{Float64},2},mask::Array{Complex{Float64},2})
+	uv = real(ifft(U.*mask,1:2)).*real(ifft(V.*mask,1:2));
 	return uv;
 end
 
 function poisson(q::Array{Float64,2},K::Array{Float64,2},L::Array{Float64,2})
 	# Uses the 2D fft to solve Laplacian(psi) = q for psi.
-	psi = real(ifft(-fft(q,1:2).*((K.^2.0+L.^2.0).^(-1.0))));
+	#psi = real(ifft(-fft(q,1:2).*((K.^2.0+L.^2.0).^(-1.0))));
+	psi = real(ifft(-fft(fft(q,1),2).*((K.^2.0+L.^2.0).^(-1.0))));
 	return psi
 end
 
@@ -84,8 +79,8 @@ end
 
 Lx = 3000.0; Ly = Lx; # km, domain size
 Lxcenter = 0.0; Lycenter = 0.0; # x & y values at the center of the grid
-Nx = 2^7; # series length (must be at least even)
-Ny = 2^7; # series length (must be at least even)
+Nx = 2^5; # series length (must be at least even)
+Ny = 2^5; # series length (must be at least even)
 dx = Lx/Float64(Nx); dy = Ly/Float64(Ny); # m, grid spacing
 x = collect(0.5*dx:dx:dx*Float64(Nx))-(Lx/2.0-Lxcenter); # m, 
 y = collect(0.5*dy:dy:dy*Float64(Ny))-(Ly/2.0-Lxcenter); # m, 
@@ -102,37 +97,37 @@ dpsidxA = -(X-Lxcenter).*psi.*(sigma^(-2.0));
 dpsidyA = -(Y-Lycenter).*psi.*(sigma^(-2.0)); 
 qA = psi.*(((X-Lxcenter).^2.0+(Y-Lycenter).^2.0).*sigma^(-4.0)-2.0*sigma^(-2.0)); 
 elseif ifield == 2 # a 2D sine wave
-kx = (2.0*pi/Lx); ky = (2.0*pi/Ly); psi = sin(X.*kx).*sin(Y.*ky);
-div_psiA = (sin(X.*kx).*cos(Y.*ky).*ky+cos(X.*kx).*sin(Y.*ky).*kx);
-dpsidxA =  kx.*cos(X.*kx).*sin(Y.*ky); dpsidyA =  ky.*sin(X.*kx).*cos(Y.*ky);
-qA = -psi.*(kx^(2.0)+ky^(2.0));
+kx = (2.0*pi/Lx); ky = (2.0*pi/Ly); u = sin(X.*kx).*sin(Y.*ky);
+divergenceA = (sin(X.*kx).*cos(Y.*ky).*ky+cos(X.*kx).*sin(Y.*ky).*kx);
+dudxA =  kx.*cos(X.*kx).*sin(Y.*ky); dudyA =  ky.*sin(X.*kx).*cos(Y.*ky);
+laplacianA = -u.*(kx^(2.0)+ky^(2.0));
 end 
 
 
 # =============================================================================
 # plots of the signal:
 
-fig = figure(); CP = contourf(X,Y,psi,200,cmap="Spectral"); 
-xlabel("x (km)"); ylabel("y (km)"); title("psi, signal"); colorbar(CP); 
+fig = figure(); CP = contourf(X,Y,u,200,cmap="RdBu"); 
+xlabel("x (km)"); ylabel("y (km)"); title("u, signal"); colorbar(CP); 
 savefig("./figures/signal.png",format="png"); close(fig);
 
 # u=-dpsi/dy, analytical
-fig = figure(); CP = contourf(X./Lx,Y./Ly,dpsidyA,200,cmap="RdBu"); 
+fig = figure(); CP = contourf(X./Lx,Y./Ly,dudyA,200,cmap="RdBu"); 
 xlabel("x"); ylabel("y"); title("signal y derivative"); colorbar(CP); 
 savefig("./figures/y_derivative_signal.png",format="png"); close(fig);
 
 # v=dpsi/dx, analytical 
-fig = figure(); CP = contourf(X./Lx,Y./Ly,dpsidxA,200,cmap="RdBu"); 
+fig = figure(); CP = contourf(X./Lx,Y./Ly,dudxA,200,cmap="RdBu"); 
 xlabel("x"); ylabel("y"); title("signal x derivative"); colorbar(CP); 
 savefig("./figures/x_derivative_signal.png",format="png"); close(fig);
 
 # divergence of psi analytical 
-fig = figure(); CP = contourf(X./Lx,Y./Ly,div_psiA,200,cmap="RdBu"); 
-xlabel("x"); ylabel("y"); title("divergence of psi analytical");
+fig = figure(); CP = contourf(X./Lx,Y./Ly,divergenceA,200,cmap="RdBu"); 
+xlabel("x"); ylabel("y"); title("divergence of u analytical");
 colorbar(CP); savefig("./figures/divergence_signal.png",format="png"); close(fig);
 
 # Laplacian of psi q=nabla2(psi) analytical 
-fig = figure(); CP = py.contourf(X./Lx,Y./Ly,qA,200,cmap="PuOr"); 
+fig = figure(); CP = contourf(X./Lx,Y./Ly,laplacianA,200,cmap="PuOr"); 
 xlabel("x"); ylabel("y"); title("Laplacian of signal"); 
 colorbar(CP); savefig("./figures/Laplacian_signal.png",format="png");
 
@@ -141,7 +136,8 @@ colorbar(CP); savefig("./figures/Laplacian_signal.png",format="png");
 # 2D FFT of psi(x,y) 
 
 # Fourier transform of psi into PSI 
-PSI = fft(psi,1:2); # 2D fft
+U = fft(u,1:2); # 2D fft
+Ux = fft(u,2); Uy = fft(u,1); # 1D fft
 
 # wavenumbers 
 k = zeros(Nx); l = zeros(Ny);
@@ -163,16 +159,16 @@ savefig("./figures/wavenumber_magnitude.png",format="png"); close(fig);
 # =============================================================================
 # Fourier domain and power spectrum plots
 
-S=(abs(PSI).*(2.0/Float64(Nx*Ny))).^2.0; S_shift = fftshift(S);
-S_vec, Kmag_vec_un = power_spectrum_2D(S,Kmag);
+P=(abs(U).*(2.0/Float64(Nx*Ny))).^2.0; 
+P_vec, Kmag_vec_un = power_spectrum_2D(P,Kmag);
 
-fig = figure(); CP = plot(Kmag_vec_un,S_vec,"b");
-xlabel("|K|"); ylabel("|PSI|"); title("2D power spectrum"); 
+fig = figure(); CP = plot(Kmag_vec_un,P_vec,"b");
+xlabel("|K|"); ylabel("|U|"); title("2D power spectrum"); 
 savefig("./figures/power_spectrum.png",format="png"); close(fig);
 
-fig = figure(); CP = contourf(K,L,S,100,cmap="gray");
+fig = figure(); CP = contourf(K,L,P,100,cmap="gray");
 xlabel("k (2pi/L) rad/km"); ylabel("l (2pi/L) rad/km"); 
-title("PSI (unshifted, no padding)"); colorbar(CP); 
+title("|U| (unshifted, no padding)"); colorbar(CP); 
 savefig("./figures/power_spectrum_2D.png",format="png"); close(fig);
 
 
@@ -180,27 +176,30 @@ savefig("./figures/power_spectrum_2D.png",format="png"); close(fig);
 # divergence by fft of psi(x,y)
 
 # first derivatives by fft
-dxpsi = real(ifft(PSI.*K.*im)); dypsi = real(ifft(PSI.*L.*im));
-div_psi = real(ifft(PSI.*(K+L).*im,1:2)); 
+dxu = real(ifft(Ux.*K.*im,2)); dyu = real(ifft(Uy.*L.*im,1));
+#divergence = real(ifft(U.*(K+L).*im,1:2)); 
+divergence = dxu+dyu;
 
 # divergence error
-div_error = abs(div_psiA-div_psi); max_div_error = maximum(abs(div_psiA-div_psi));
+divergence_error = abs(divergenceA-divergence); max_div_error = maximum(divergence_error);
 println("The maximum divergence computation error is $(max_div_error) for a $Nx by $Ny grid.\n")
 
 # first derivative plots:
 
+#=
 # u=-dpsi/dy, error 
 fig = figure(); CP = contourf(X./Lx,Y./Ly,abs(dypsi-dpsidyA),200,cmap="gray"); 
 xlabel("x"); ylabel("y"); title("y derivative, error"); colorbar(CP); 
 savefig("./figures/y_derivative_error.png",format="png"); close(fig);
 
 # v=dpsi/dx, error
-fig = figure(); CP = py.contourf(X./Lx,Y./Ly,abs(dxpsi-dpsidxA),200,cmap="gray"); 
+fig = figure(); CP = contourf(X./Lx,Y./Ly,abs(dxpsi-dpsidxA),200,cmap="gray"); 
 xlabel("x"); ylabel("y"); title("x derivative, error"); colorbar(CP); 
 savefig("./figures/x_derivative_error.png",format="png"); close(fig);
+=#
 
 # divergence of psi error
-fig = figure(); CP = py.contourf(X./Lx,Y./Ly,abs(div_psiA-div_psi),200,cmap="gray");
+fig = figure(); CP = contourf(X./Lx,Y./Ly,divergence_error,200,cmap="gray");
 xlabel("x"); ylabel("y"); title("divergence, error");
 colorbar(CP); savefig("./figures/divergence_error.png",format="png"); close(fig);
 
@@ -209,15 +208,16 @@ colorbar(CP); savefig("./figures/divergence_error.png",format="png"); close(fig)
 # Laplacian by fft of psi(x,y)
 
 # the Laplacian by inverse fft
-q = real(ifft(-PSI.*(K.^2.0+L.^2.0),1:2));
+laplacian = real(ifft(-Ux.*K.^2.0,2)+ifft(-Uy.*L.^2.0,1));
+#laplacian = real(ifft(-U.*(K.^2.0+L.^2.0),1:2));
 
 # maximum error
-lap_error = abs(qA-q); max_lap_error = maximum(abs(qA-q));
+laplacian_error = abs(laplacianA-laplacian); max_lap_error = maximum(laplacian_error);
 println("The maximum Laplacian computation error is $(max_lap_error) for a $Nx by $Ny grid.\n")
 
 # plot of the real component, computational error 
-fig = figure(); CP = py.contourf(X./Lx,Y./Ly,abs(qA-q),200,cmap="gray");
-xlabel("x"); ylabel("y"); title("Laplacian of psi error"); 
+fig = figure(); CP = contourf(X./Lx,Y./Ly,laplacian_error,200,cmap="gray");
+xlabel("x"); ylabel("y"); title("Laplacian error"); 
 colorbar(CP); savefig("./figures/Laplacian_error.png",format="png");
 
 
@@ -234,10 +234,18 @@ ua = exp(-((X-Lxcenter).^2+(Y-Lycenter).^2)./(2.0*sigma^2))+rand(size(X)).*0.5;
 ub = exp(-((X-Lxcenter).^2+(Y-Lycenter).^2)./(2.0*sigma^2))+rand(size(X)).*0.5;
 end
 
+# mask for 2/3 rule padding for de-aliasing a quadratic signal via fft
+mask = ones(size(Kmag))+ones(size(Kmag)).*im;
+for j = 1:(size(Kmag,1)*size(Kmag,2)) # 2/3 rule
+	if abs(Kmag[j]) >= max(size(Kmag,1),size(Kmag,2))/3.0*Kmag[1,2]; 
+		mask[j] = 0.0+0.0im; # Fourier space variables
+	end
+end
+
 # aliased and de-aliased quadratic signal
 u2_alias = ua.*ub; # aliased square
 Ua = fft(ua,1:2); Ub = fft(ub,1:2);
-tic(); u2_dealias = dealias(Ua,Ub,Kmag); time = toq(); # de-aliased square
+tic(); u2_dealias = dealias(Ua,Ub,mask); time = toq(); # de-aliased square
 println("The computation time is for the de-aliased signal is $time seconds for a $Nx by $Ny grid.\n")
 
 # fft of the quadratic signal
@@ -258,11 +266,11 @@ xlabel("k"); ylabel("|PSI|"); title("2D power spectrum");
 axis([minimum(Hmag_vec1),maximum(Hmag_vec1),0.0,maximum(S1_vec)/2.0]);
 savefig("./figures/quadratic_signal_power_spectrum.png",format="png"); close(fig);
 
-fig = figure(); CP = py.contourf(X./Lx,Y./Ly,u2_alias,200,cmap="PuOr"); 
+fig = figure(); CP = contourf(X./Lx,Y./Ly,u2_alias,200,cmap="PuOr"); 
 xlabel("x"); ylabel("y"); title("u*u aliased"); 
 colorbar(CP); savefig("./figures/quadratic_signal_aliased.png",format="png"); 
 
-fig = figure(); CP = py.contourf(X./Lx,Y./Ly,u2_dealias,200,cmap="PuOr"); 
+fig = figure(); CP = contourf(X./Lx,Y./Ly,u2_dealias,200,cmap="PuOr"); 
 xlabel("x"); ylabel("y"); title("u*u de-aliased"); 
 colorbar(CP); savefig("./figures/quadratic_signal_dealiased.png",format="png");
 
@@ -271,16 +279,16 @@ colorbar(CP); savefig("./figures/quadratic_signal_dealiased.png",format="png");
 # Poisson equation solution by fft
 
 # Poisson equation solution: Laplacian(psi) = qA
-tic(); psiP = poisson(qA,Kinv,Linv); time = toq(); 
+tic(); uP = poisson(laplacianA,Kinv,Linv); time = toq(); 
 println("The first Poisson equation computation time is $(time) seconds for a $Nx by $Ny grid.\n")
-Poisson_error = abs(psiP-psi); # Poisson equation solution error
+Poisson_error = abs(uP-u); # Poisson equation solution error
 max_err = maximum(Poisson_error);
 
-fig = figure(); CP = py.contourf(X./Lx,Y./Ly,psiP,200,cmap="Spectral");
+fig = figure(); CP = contourf(X./Lx,Y./Ly,uP,200,cmap="Spectral");
 xlabel("x"); ylabel("y"); title("Laplacian(psi) = q, psi solution"); 
 colorbar(CP); savefig("./figures/Poisson_solution.png",format="png"); 
 
-fig = figure(); CP = py.contourf(X./Lx,Y./Ly,Poisson_error,200,cmap="gray")
+fig = figure(); CP = contourf(X./Lx,Y./Ly,Poisson_error,200,cmap="gray")
 xlabel("x"); ylabel("y"); title("Laplacian(psi) = q, psi solution error"); 
 colorbar(CP); savefig("./figures/Poisson_solution_error.png",format="png"); 
 
@@ -295,7 +303,107 @@ qA2 = psi2.*(((X-Lxcenter).^2.0+(Y-Lycenter).^2.0).*sigma^(-4.0)-2.0*sigma^(-2.0
 tic(); psiP2 = poisson(qA2,Kinv,Linv); time = toq(); 
 println("The second Poisson equation computation time is $time seconds for a $Nx by $Ny grid.\n")
 
-fig = figure(); CP = py.contourf(X./Lx,Y./Ly,psiP2,200,cmap="Spectral");
+fig = figure(); CP = contourf(X./Lx,Y./Ly,psiP2,200,cmap="RdBu");
 xlabel("x"); ylabel("y"); title("Laplacian(psi) = q-By, psi solution"); 
 colorbar(CP); savefig("./figures/Poisson_solution_linear_slope.png",format="png");
+
+
+# =============================================================================
+# fft plan speed test
+
+m_end = 1000;
+nx = collect(4:10); # typeof(n) = Array{Int64,1}, powers of 2 grid resolution
+ny = collect(4:10);
+
+# initialized fields
+fft_time = zeros(length(nx)); fft_plan_time = zeros(length(nx));
+Linf_div = zeros(length(nx)); Linf_divP = zeros(length(nx));
+Linf_lap = zeros(length(nx)); Linf_lapP = zeros(length(nx));
+
+for m = 1:length(nx) # loop over powers of 2 grid resolution 
+
+# domain
+Lx = 3000.0; Ly = Lx; # km, domain size
+Lxcenter = 0.0; Lycenter = 0.0; # x & y values at the center of the grid
+Nx = 2^(nx[m]); # series length (must be at least even)
+Ny = 2^(ny[m]); # series length (must be at least even)
+dx = Lx/Float64(Nx); dy = Ly/Float64(Ny); # m, grid spacing
+x = collect(0.5*dx:dx:dx*Float64(Nx))-(Lx/2.0-Lxcenter); # m, 
+y = collect(0.5*dy:dy:dy*Float64(Ny))-(Ly/2.0-Lxcenter); # m, 
+X,Y = meshgrid(x,y); 
+
+# signal
+kx = (2.0*pi/Lx); ky = (2.0*pi/Ly); u = sin(X.*kx).*sin(Y.*ky);
+divergenceA = (sin(X.*kx).*cos(Y.*ky).*ky+cos(X.*kx).*sin(Y.*ky).*kx);
+dudxA =  kx.*cos(X.*kx).*sin(Y.*ky); dudyA =  ky.*sin(X.*kx).*cos(Y.*ky);
+laplacianA = -u.*(kx^(2.0)+ky^(2.0));
+
+# wavenumbers 
+k = zeros(Nx); l = zeros(Ny);
+k[2:Int64(Nx/2)+1] = collect(1:Int64(Nx/2)).*(2.0*pi/Lx); # rad/m
+k[Int64(Nx/2)+2:Nx] = -collect(Int64(Nx/2)-1:-1:1).*(2.0*pi/Lx); # rad/m
+l[2:Int64(Ny/2)+1] = collect(1:Int64(Ny/2)).*(2.0*pi/Ly); # rad/m
+l[Int64(Ny/2)+2:Ny] = -collect(Int64(Ny/2)-1:-1:1).*(2.0*pi/Ly); # rad/m
+K,L = meshgrid(k,l); 
+Kmag = (K.^2.0+L.^2.0).^(1.0/2.0); # wavenumber magnitude
+
+# initialize spectral u, derivatives
+U = zeros(size(X)); dudx = zeros(size(X)); d2udx2 = zeros(size(X));
+Up = zeros(size(X)); dudxp = zeros(size(X)); d2udx2p = zeros(size(X));
+
+# spectral u, derivatives via standard fft: wall time test
+tic(); for m1 = 1:m_end;  
+Ux = fft(u,2); Uy = fft(u,1); # 1D fft
+divergence = real(ifft(Ux.*K.*im,2))+real(ifft(Uy.*L.*im,1));
+laplacian = real(ifft(-Ux.*K.^2.0,2)+ifft(-Uy.*L.^2.0,1));
+end; fft_time[m] = toq();
+
+# multithread number of threads and fft_plan:
+FFTW.set_num_threads(4)
+PUx = plan_fft(u,2,flags=FFTW.EXHAUSTIVE); # 1D fft
+PUy = plan_fft(u,1,flags=FFTW.EXHAUSTIVE); # 1D fft 
+UxP = PUx*u; Uyp = PUy*u; 
+Pdx = plan_ifft(Ux.*K.*im,2,flags=FFTW.EXHAUSTIVE);
+Pdy = plan_ifft(Uy.*L.*im,1,flags=FFTW.EXHAUSTIVE);
+divergenceP = real(Pdx*(Ux.*K.*im)+Pdy*(Uy.*L.*im)); 
+Pdx2 = plan_ifft(-Ux.*K.^2.0,2,flags=FFTW.EXHAUSTIVE);
+Pdy2 = plan_ifft(-Uy.*L.^2.0,1,flags=FFTW.EXHAUSTIVE);
+laplacianP = real(Pdx2*(-Ux.*K.^2.0)+Pdy2*(-Uy.*L.^2.0)); 
+
+# spectral u, derivatives via fft plan: wall time test
+tic(); for m2 = 1:m_end; 
+UxP = PUx*u; Uyp = PUy*u; 
+divergenceP = real(Pdx*(Ux.*K.*im)+Pdy*(Uy.*L.*im));
+laplacianP = real(Pdx2*(-Ux.*K.^2.0)+Pdy2*(-Uy.*L.^2.0)); 
+end; fft_plan_time[m] = toq();
+
+# infinity norm error for fft and fft plan 
+Linf_div[m] = maximum(abs(divergence-divergenceA));
+Linf_lap[m] = maximum(abs(laplacian-laplacianA));
+Linf_divP[m] = maximum(abs(divergenceP-divergenceA));
+Linf_lapP[m] = maximum(abs(laplacianP-laplacianA));
+
+end
+
+Nvec = (ones(length(nx)).*2.0).^nx;
+NlogN = (log2(Nvec).*Nvec).*((fft_time[1]/(log2(Nvec[1]).*Nvec[1]))*10.0);
+
+# wall time plot
+fig = figure(); 
+loglog(Nvec.^2.0,NlogN,"k--",label="Nlog2(N)");
+loglog(Nvec.^2.0,fft_time,"r",label="2D fft"); 
+loglog(Nvec.^2.0,fft_plan_time,"b",label="2D fft plan"); legend(loc=2);
+xlabel("N^2"); ylabel("wall time, seconds"); title("$m_end divergence/Laplacian calculations"); 
+savefig("./figures/fft_plan_computation_time.png",format="png"); close(fig);
+
+# error plot
+fig = figure(); 
+loglog(Nvec.^2.0,Linf_div,"k",label="divergence"); 
+loglog(Nvec.^2.0,Linf_lap,"g",label="Laplacian"); 
+loglog(Nvec.^2.0,Linf_divP,"b--",label="divergence, plan");
+loglog(Nvec.^2.0,Linf_lapP,"r--",label="Laplacian, plan"); 
+xlabel("N^2"); ylabel("L infinity"); title("2D fft error"); legend(loc=4);
+savefig("./figures/fft_plan_error.png",format="png"); close(fig)
+
+println("done!")
 
